8e6553fa706773f4c6bf205a7220fd60541cfba9
Amanda Dourado <amanda_lopes1998@hotmail.com>
2023-02-15 21:02:59 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
+		SpringApplication.run(CourseApplication.class, args);		
 	}
 
+	
 }
---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	
+	// essa anotação faz o spring fazer a injeção de dependencia automatica
+	@Autowired
+	private UserRepository userRepository;
+	
+	// metodo herdado do ComandLineRunner
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAll(Arrays.asList(u1,u2));
+	}
+}
